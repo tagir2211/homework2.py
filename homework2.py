bf51b383f6d1c52041f5ca0b9f46f2f17bdb3dfe
tagir2211 <tagir2211@gmail.com>
2024-07-29 22:43:13 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+number_of_completed_HW = 12
+number_of_hours_spend = 1.5
+name_course = 'Python'
+tami_for_1_task = number_of_hours_spend/number_of_completed_HW
+print('Курс: ' + name_course + ', всего задач: ' + str(number_of_completed_HW) + ', затрачено часов: '+ str(number_of_hours_spend)+', среднее время выполнения: '+ str(tami_for_1_task) + ' часа.')
